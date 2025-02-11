9e3d9c7942b8cec087ca75c1853672ff0c58c995
IceMoonMagic <icemoonmagic@gmail.com>
2025-02-11 16:11:47 -0600

    Refactored  to (generally) avoid editable children

--- maze/maze2d/options/line_options.gd
+++ maze/maze2d/options/line_options.gd
@@ -1,6 +1,30 @@
+@tool
 class_name LineOptionControl
 extends Control
 
+@export var header: String = "Header":
+	set(val):
+		header = val
+		if Engine.is_editor_hint():
+			$Label.text = header
+@export var default_enabled := true:
+	set(enabled):
+		default_enabled = enabled
+		if Engine.is_editor_hint():
+			default_options.enabled = enabled
+			enabled_button.button_pressed = enabled
+@export var default_color := Color.BLACK:
+	set(color):
+		default_color = color
+		if Engine.is_editor_hint():
+			default_options.color = color
+			color_picker.color = color
+@export var default_thickness := 1.0:
+	set(thickness):
+		default_thickness = thickness
+		if Engine.is_editor_hint():
+			default_thickness = thickness
+			thickness_slider.value = thickness
 @onready var enabled_button: CheckButton = $Enabled/ToggleButton
 @onready var enabled_reset_button: Button = $Enabled/ResetButton
 @onready var color_picker: ColorPickerButton = $Color/ColorPicker
@@ -11,19 +35,23 @@ extends Control
 ## Default options set in the inspector
 ## DO NOT MODIFY
 @onready var default_options := MazeData.LineOptionData.new(
-	enabled_button.button_pressed, color_picker.color, thickness_slider.value
+	default_enabled, default_color, default_thickness
 )
 
 ## Options actively in use
 @onready var applied_options := MazeData.LineOptionData.new(
-	enabled_button.button_pressed, color_picker.color, thickness_slider.value
+	default_enabled, default_color, default_thickness
 )
-# ToDo: Save options to file
 
 ## Modified but unsaved options
 @onready var unapplied_options := MazeData.LineOptionData.new(
-	enabled_button.button_pressed, color_picker.color, thickness_slider.value
+	default_enabled, default_color, default_thickness
 )
+
+
+func _ready() -> void:
+	$Label.text = header
+	set_to(default_options)
 
 
 func set_to(option_data: MazeData.LineOptionData) -> void:
