98e50aa50339f97deb23d7f1fc28781863679b8c
IceMoonMagic <icemoonmagic@gmail.com>
2026-02-01 19:16:32 -0600

    Refactored / reworked how the derivations work.
    - Each export preset in each of pack, debug, and release export modes are now derivations
    - Each of these derivations' outputs is just the output of the export process
      - ie, not in any directories like bin, libexec, or share
    - Each of pack, debug, and release have an aggregated derivation
      - $out is every export preset for said export mode in subdirs
    - Default package set to aggregated release (so builds all release presets)
    - Put key package details at top, to help in potentially using in other projects

--- flake.nix
+++ flake.nix
@@ -9,10 +9,105 @@
       gameName = "maze-game";
       gameVersion = "0.2.0";
       godotPackages = pkgs: pkgs.godotPackages_4_4;
+      exportPresets = [
+        (mkExportPreset "Linux" "")
+        (mkExportPreset "Linux 2D" "")
+        (mkExportPreset "Web" ".html")
+        (mkExportPreset "Web 2D" ".html")
+        (mkExportPreset "Windows Desktop" ".exe")
+        (mkExportPreset "Windows Desktop 2D" ".exe")
+      ];
+
       builderDir = ".builder";
       supportedSystems = [ "x86_64-linux" ];
+
       forEachSystem = nixpkgs.lib.genAttrs supportedSystems;
       getPkgs = system: nixpkgs.legacyPackages.${system};
+      mkExportPreset = name: ext: {
+        presetName = name;
+        outputExtension = ext;
+      };
+
+      allPacks = builtins.map (
+        preset:
+        preset
+        // {
+          exportMode = "pack";
+          outputExtension = ".pck";
+        }
+      ) exportPresets;
+      allDebugs = builtins.map (preset: preset // { exportMode = "debug"; }) exportPresets;
+      allReleases = builtins.map (preset: preset // { exportMode = "release"; }) exportPresets;
+
+      mkExports =
+        presets: pkgs:
+        builtins.map (p: {
+          name = "${gameName}-${p.presetName}-${p.exportMode}";
+          value = mkDerivation p pkgs;
+        }) presets;
+
+      mkDerivation =
+        preset: pkgs:
+        pkgs.stdenv.mkDerivation (
+          let
+            inherit (preset) presetName outputExtension exportMode;
+            godot = (godotPackages pkgs).godot;
+            export-templates-bin = (godotPackages pkgs).export-templates-bin;
+          in
+          {
+            pname = gameName;
+            version = gameVersion;
+            src = ./.;
+            nativeBuildInputs = [
+              godot
+              export-templates-bin
+            ];
+
+            # Kind of a hack becuase Godot doesn't have a way to configure an export template directory
+            # See https://github.com/godotengine/godot-proposals/issues/2565
+            configurePhase = ''
+              runHook preConfigure
+
+              mkdir -p "${builderDir}/editor_data"
+              mkdir -p ${builderDir}/dist
+              # Enable Godot's Self Contained Mode
+              touch "${builderDir}/_sc_"
+              # Self Contained won't work if the binary is just symlinked, so it needs to be copied
+              install -Dm755 ${godot}/libexec/godot.linuxbsd.editor.x86_64 -T ${builderDir}/godot
+              # Can and will link templates because they're almost 2 GiBs
+              # To my knowledge, symlinking like this should be fine.
+              ln -s "${export-templates-bin}/share/godot/export_templates" "${builderDir}/editor_data/"
+
+              runHook postConfigure
+            '';
+
+            buildPhase = ''
+              runHook preBuild
+              "./${builderDir}/godot" --headless --export-${exportMode} "${presetName}" "${builderDir}/dist/${gameName}${outputExtension}"
+              runHook postBuild
+            '';
+
+            installPhase = ''
+              runHook preInstall
+              cp "${builderDir}/dist/" -r -T "$out/"
+              runHook postInstall
+            '';
+          }
+        );
+      mkAggregation =
+        name: mkExport: pkgs:
+        let
+          exports = mkExport pkgs;
+        in
+        pkgs.stdenv.mkDerivation {
+          pname = "${gameName}-name";
+          version = gameVersion;
+          src = pkgs.emptyDirectory;
+          buildInputs = builtins.map (e: e.value) exports;
+          installPhase = builtins.concatStringsSep "\n" (
+            [ "mkdir -p $out" ] ++ (builtins.map (e: "ln -s \"${e.value}\" -T \"$out/${e.name}\"") exports)
+          );
+        };
     in
     {
       formatter = forEachSystem (system: (getPkgs system).nixfmt-tree);
@@ -21,82 +116,18 @@
           buildInputs = [ (godotPackages (getPkgs system)).godot ];
         };
       });
-      packages = {
-        x86_64-linux.default = self.packages.x86_64-linux.${gameName};
-        x86_64-linux.${gameName} =
-          let
-            pkgs = getPkgs "x86_64-linux";
-            godot = (godotPackages pkgs).godot;
-            export-templates-bin = (godotPackages pkgs).export-templates-bin;
-            outputExtention = ".x86_64";
-          in
-          pkgs.stdenv.mkDerivation (
-            (self.builder {
-              presetName = "Linux";
-              inherit outputExtention;
-              exportMode = "release";
-            } pkgs)
-            // {
-              buildInputs = with pkgs; [
-                bash
-                steam-run-free
-              ];
-              installPhase = ''
-                # The export templates still assume some dynamic linking, so need to make a wrapper with steam-run
-                # the shell wrappers don't seem to support this use case (passing the executable through another program)
-                # May also try just adding all dependancies. Best lead is for compiling the engine itself.
-                # https://docs.godotengine.org/en/latest/engine_details/development/compiling/compiling_for_linuxbsd.html
-                install -Dm755 "${gameName}${outputExtention}" "$out/libexec/${gameName}${outputExtention}"
-                mkdir $out/bin
-                echo \#!${pkgs.bash}/bin/bash >> "$out/bin/${gameName}"
-                echo ${pkgs.steam-run-free}/bin/steam-run "$out/libexec/${gameName}${outputExtention}" >> "$out/bin/${gameName}"
-                chmod +rx "$out/bin/${gameName}"
-              '';
-            }
-          );
-      };
-      builder =
-        {
-          presetName,
-          outputExtention,
-          exportMode,
-        }:
-        pkgs:
+      packages = forEachSystem (
+        system:
         let
-          godot = (godotPackages pkgs).godot;
-          export-templates-bin = (godotPackages pkgs).export-templates-bin;
+          pkgs = getPkgs system;
         in
         {
-          pname = gameName;
-          version = gameVersion;
-          src = ./.;
-          nativeBuildInputs = [
-            godot
-            export-templates-bin
-          ];
-
-          # Kind of a hack becuase Godot doesn't have a way to configure an export template directory
-          # See https://github.com/godotengine/godot-proposals/issues/2565
-          configurePhase = ''
-            runHook preConfigure
-
-            mkdir -p "${builderDir}/editor_data"
-            # Enable Godot's Self Contained Mode
-            touch "${builderDir}/_sc_"
-            # Self Contained won't work if the binary is just symlinked, so it needs to be copied
-            install -Dm755 ${godot}/libexec/godot.linuxbsd.editor.x86_64 -T ${builderDir}/godot
-            # Can and will link templates because they're almost 2 GiBs
-            # To my knowledge, symlinking like this should be fine.
-            ln -s "${export-templates-bin}/share/godot/export_templates" "${builderDir}/editor_data/"
-
-            runHook postConfigure
-          '';
-
-          buildPhase = ''
-            runHook preBuild
-            "./${builderDir}/godot" --headless --export-${exportMode} "${presetName}" "${gameName}${outputExtention}"
-            runHook postBuild
-          '';
-        };
+          "${gameName}-pack" = mkAggregation "pack" (mkExports allPacks) pkgs;
+          "${gameName}-debug" = mkAggregation "debug" (mkExports allDebugs) pkgs;
+          "${gameName}-release" = mkAggregation "release" (mkExports allReleases) pkgs;
+          default = self.packages.${system}."${gameName}-release";
+        }
+        // builtins.listToAttrs (mkExports (allPacks ++ allDebugs ++ allReleases) (getPkgs system))
+      );
     };
 }
